ba5890d9270bdf265255157a40f2f65c786ed770
zqsun0 <shoshinn95@gmail.com>
2021-09-13 11:28:18 +0900

    an example

---
+++ src/component/CharacterCounter.jsx
@@ -0,0 +1,13 @@
+import React  from 'react';
+import TextInput from './TextInput';
+
+
+export default function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      {/*<CharacterCount />*/}
+
+    </div>
+  );
+}
---
+++ src/component/TextInput.jsx
@@ -0,0 +1,19 @@
+import React  from 'react';
+import {useRecoilState} from 'recoil';
+import {textState} from '../store/atom';
+
+export default function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
